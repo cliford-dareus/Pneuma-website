eb30e3a7eee9cbd77ab0af4806a848f4ada7b889
cliford Dareus <104238549+cliford-dareus@users.noreply.github.com>
2023-04-02 21:01:51 -0400

    Add content slider and links icon

---
+++ src/components/slider/index.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useRef, useState } from 'react';
+import styles from './slider.module.css';
+
+const index = () => {
+    const [ Index, setIndex ] = useState<number>(1)
+    const slide = [1,2,3,4];
+    const timeoutRef = useRef<any>(null);
+
+    function resetTimeout() {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+    }
+  
+    React.useEffect(() => {
+      resetTimeout();
+      timeoutRef.current = setTimeout(
+        () =>
+          setIndex((prevIndex) =>
+            prevIndex === slide.length - 1 ? 0 : prevIndex + 1
+          ),
+        5000
+      );
+  
+      return () => {
+        resetTimeout();
+      };
+    }, [Index]);
+  return (
+    <div className={styles.container}>
+        <div className={styles.slider}>
+           {slide.map((x, index)=> {
+            return(
+                <div 
+                    className={`${styles.slide} ${Index === index? styles.active: ''}`}
+                >
+                    <span>{x}</span>
+                </div>
+            )
+            })} 
+        </div>
+        
+    </div>
+  )
+}
+
+export default index;
--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
 import styles from './HomePage.module.css';
 import { motion } from 'framer-motion';
+import Slider from '../../components/slider';
+import taxImage from '../../assets/taxImage.jpg';
+import insIMage from '../../assets/pexels-mikhail-nilov-7735637.jpg';
+import { BsHospital } from 'react-icons/bs';
+import { FaUmbrellaBeach, FaStethoscope, FaHandHoldingHeart } from 'react-icons/fa';
 
 const HomePage = () => {
   return (
@@ -10,30 +15,50 @@ const HomePage = () => {
       <motion.div >
         <div className={styles.grid}>
           <div className={styles.gridOne}>
-            <h1>Life Insurance Done The Right Way.</h1>
+            <h1>Life Insurance Done The Right Way...</h1>
           </div>
 
           <div className={styles.gridTwo}>
-            <img src="" alt="" />
+            <img src={insIMage} alt="" />
+            <img src={taxImage} alt="" />
+            <img src={taxImage} alt="" />
+            <img src={taxImage} alt="" />
+            <img src={taxImage} alt="" />
           </div>
 
-          <div className=''>three</div>
-          <div className=''>four</div>
+          <div className={styles.gridThree}>
+            <Slider />
+          </div>
 
-          <div className={styles.services}>
+          <div className={styles.gridFour}>
             <h2>Our Services</h2>
+          </div>
+        
+          <div className={styles.services}>
+            {/* <h2>Our Services</h2> */}
             <div className={styles.servicesCard}>
               <div>
-                <span>&</span>
-                <h3>LIfe Insurance with Living Benefit</h3>
+                <span>
+                  <FaHandHoldingHeart />
+                </span>
+                <h3>Life Insurance with Living Benefit</h3>
               </div>
               <div>
-                <h3>LIfe Insurance with Hospital Benefit</h3>
+                <span>
+                  <BsHospital />
+                </span>
+                <h3>Life Insurance with Hospital Benefit</h3>
               </div>
               <div>
+                <span>
+                  <FaStethoscope />
+                </span>
                 <h3>ObamaCare Health Insurance</h3>
               </div>
               <div>
+                <span>
+                  <FaUmbrellaBeach />
+                </span>
                 <h3>Retirement Investment</h3>
               </div>
             </div>
